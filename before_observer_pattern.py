5a6006bf2ed03edb4eb13531526feda8b3da2a30
Alex Ryan <alex.ryan@inspectorio.com>
2022-09-13 13:01:15 +0700

    Refactor

--- before_observer_pattern.py
+++ before_observer_pattern.py
@@ -11,28 +11,43 @@ class Account:
 
 
 class AccountClient:
+
+    def configure_test_mode(self, enabled: bool):
+        self.test_mode = enabled
     
     def register_account(self, name, email):
         account = Account(name=name, email=email)
-        print(f"Account created: {account}")
-        print(f"Emailing {account.email} a verification code.")
-        return account
+        print(f"\nAccount created: {account}")
 
+        if not self.test_mode:
+            print(f"Emailing {account.email} a verification code.")
+        
+        return account
 
     def upgrade_account(self, account: Account):
         account.plan = 2
         print(f"\nAccount upgraded: {account}")
-        print(f"Emailing {account.email} their receipt.")
-        print(f"Sending a Slack message to Accounting; {account.email} upgraded their account.")
+
+        if not self.test_mode:
+            print(f"Emailing {account.email} their receipt.")
+            print(f"Sending a Slack message to Accounting; {account.email} upgraded their account.")
+        
         return account
 
 
 def main():
+    print("CREATE FIRST ACCOUNT...")
     account_client = AccountClient()
-
+    account_client.configure_test_mode(False)
     account = account_client.register_account('John Doe', "john.doe@foo.com")
-
     account_client.upgrade_account(account)
+
+    # Perhaps your user enters "test mode" in which no notifications should be sent
+    # In this case, we can unsubscribe the listeners at runtime
+    print("\n\nCREATE SECOND (TEST) ACCOUNT...")
+    account_client.configure_test_mode(True)
+    test_account = account_client.register_account('test', "testk@test.com")
+    account_client.upgrade_account(test_account)
 
 
 if __name__ == '__main__':
--- observer_pattern.py
+++ observer_pattern.py
@@ -62,15 +62,26 @@ class EventManager:
 
 
 class AccountClient:
-    def __init__(self, event_manager: EventManager) -> None:
-        self.event_manager = event_manager
+    def __init__(self) -> None:
+        self.event_manager = EventManager()
+        self.configure_test_mode(False)
+    
+    def configure_test_mode(self, enabled):
+        if not enabled:
+            self.event_manager.subscribe(Events.REGISTER, CreatedVerificationEmailListener)
+            self.event_manager.subscribe(Events.UPGRADE, UpgradedReceiptEmailListener)
+            self.event_manager.subscribe(Events.UPGRADE, UpgradedAccountingSlackListener)
+        else:
+            print("\nEntering Testing mode...")
+            self.event_manager.unsubscribe(Events.REGISTER, CreatedVerificationEmailListener)
+            self.event_manager.unsubscribe(Events.UPGRADE, UpgradedReceiptEmailListener)
+            self.event_manager.unsubscribe(Events.UPGRADE, UpgradedAccountingSlackListener)
     
     def register_account(self, name, email):
         account = Account(name=name, email=email)
         print(f"\nAccount created: {account}")
         self.event_manager.notify(Events.REGISTER, account)
         return account
-
 
     def upgrade_account(self, account: Account):
         account.plan = 2
@@ -80,25 +91,18 @@ class AccountClient:
 
 
 def main():
-    event_manager = EventManager()
-    event_manager.subscribe(Events.REGISTER, CreatedVerificationEmailListener)
-    event_manager.subscribe(Events.UPGRADE, UpgradedReceiptEmailListener)
-    event_manager.subscribe(Events.UPGRADE, UpgradedAccountingSlackListener)
-
-    account_client = AccountClient(event_manager)
+    print("CREATE FIRST ACCOUNT...")
+    account_client = AccountClient()
+    account_client.configure_test_mode(False)
     account = account_client.register_account('John Doe', "john.doe@foo.com")
     account_client.upgrade_account(account)
 
     # Perhaps your user enters "test mode" in which no notifications should be sent
     # In this case, we can unsubscribe the listeners at runtime
-    print("\nEntering Testing mode...")
-    event_manager.unsubscribe(Events.REGISTER, CreatedVerificationEmailListener)
-    event_manager.unsubscribe(Events.UPGRADE, UpgradedReceiptEmailListener)
-    event_manager.unsubscribe(Events.UPGRADE, UpgradedAccountingSlackListener)
-
-    account_client = AccountClient(event_manager)
-    account = account_client.register_account('Tim Cook', "tim.cook@bar.com")
-    account_client.upgrade_account(account)
+    print("\n\nCREATE SECOND (TEST) ACCOUNT...")
+    account_client.configure_test_mode(True)
+    test_account = account_client.register_account('test', "testk@test.com")
+    account_client.upgrade_account(test_account)
 
 
 if __name__ == '__main__':
